3961f64926ce0bd92dd790e2100d398ffe404b94
Bhattiprolu Harika <bhattiprolu.harika@mindtree.com>
2022-10-18 11:23:18 +0000

    changes

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -3,7 +3,10 @@
 namespace WishList.Controllers
 {
     public class HomeController:Controller
-    {
+    {/// <summary>
+    /// Index
+    /// </summary>
+    /// <returns></returns>
         public ActionResult Index()
         {
             return View("Index");
